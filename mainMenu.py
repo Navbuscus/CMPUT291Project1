6e3878cf1c5c9b5d31505a97cae35507f39a7abe
Navjeet <nsd@ualberta.ca>
2015-03-10 15:54:54 -0600

    put false login data

--- mainMenu.py
+++ mainMenu.py
@@ -82,7 +82,7 @@ menu_actions = {
 }
 
 user = 'ajwu'
-pw = 'a1__5LoYz'   
+pw = 'a1__5LoY'   
 connStr=''+user+'/' + pw +'@gwynne.cs.ualberta.ca:1521/CRS'  
 try:
     connection  = cx_Oracle.connect(connStr)
