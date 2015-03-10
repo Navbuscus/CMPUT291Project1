5d2d0c408457094dc6607f3f1afd175f40d922cc
Frost Byte <frostbyte@uws146906usr.uws.ualberta.ca>
2015-03-10 14:46:44 -0600

    fix silly error

--- mainMenu.py
+++ mainMenu.py
@@ -105,7 +105,7 @@ pw = 'a1__5LoYz'
 connStr=''+user+'/' + pw +'@gwynne.cs.ualberta.ca:1521/CRS'  
 try:
     connection  = cx_Oracle.connect(connStr)
-    curs = connection.cursor() 
+    cursor = connection.cursor() 
     main_menu()
 except cx_Oracle.DatabaseError as exc:
     error, = exc.args
