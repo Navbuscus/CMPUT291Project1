287299de62dde963bb4761ce6e350850d630809f
anoyaj <ajwu@ualberta.ca>
2015-03-27 12:59:40 -0600

    search engine fix

--- SE.py
+++ SE.py
@@ -22,7 +22,7 @@ def driverName():
             else:
                 mainMenu.cursor.execute("SELECT d.licence_no FROM drive_licence d, people p WHERE p.sin = d.sin AND  p.name = '%s'" %name)
                 data = mainMenu.cursor.fetchall()                   
-                driver_descript()
+                descript()
                 for unique_licence_no in data:
                     mainMenu.cursor.execute("SELECT DISTINCT p.name, d.licence_no, p.addr, p.birthday, d.class, c.description, d.expiring_date FROM drive_licence d, people p, restriction r, driving_condition c WHERE d.licence_no = %s AND p.sin = d.sin AND r.licence_no = d.licence_no AND r.r_id = c.c_id" % unique_licence_no)
                     data = mainMenu.cursor.fetchall()
